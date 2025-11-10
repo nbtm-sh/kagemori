111c09e96a332d641f7fcb7914461f1101f90782
nbtm-sh <dog@nbtm.sh>
2025-11-10 13:18:30 +1100

    feat(end_date): Add end date to job state

--- user-server/libkage/queue/state.py
+++ user-server/libkage/queue/state.py
@@ -1,5 +1,5 @@
 class JobState:
-    def __init__(self, job_id=None, job_state=None, job_tmp_directory=None, job_uuid=None, job_certificate=None, job_owner=None, job_node=None, gui_state=None):
+    def __init__(self, job_id=None, job_state=None, job_tmp_directory=None, job_uuid=None, job_certificate=None, job_owner=None, job_node=None, gui_state=None, end_date=None):
         self.job_id = job_id
         self.job_state = job_state
         self.job_tmp_directory = job_tmp_directory
@@ -8,6 +8,7 @@ class JobState:
         self.job_owner = job_owner
         self.job_node = job_node
         self.gui_state = gui_state
+        self.end_date = end_date
 
     def serialise(self):
         return {
@@ -17,5 +18,6 @@ class JobState:
             "job_uuid": self.job_uuid,
             "job_owner": self.job_owner,
             "job_node": self.job_node,
-            "gui_state": self.gui_state
+            "gui_state": self.gui_state,
+            "end_date": self.end_date.isoformat() if self.end_date is not None else None
         }
